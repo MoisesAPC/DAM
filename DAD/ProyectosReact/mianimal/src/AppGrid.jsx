98bdb1b44cb6c15121106fa859d1544421bcf6b7
moisesPC <moisesapc64@gmail.com>
2024-09-24 10:36:18 +0100

    more

--- DAD/ProyectosReact/mianimal/src/AppGrid.jsx
+++ DAD/ProyectosReact/mianimal/src/AppGrid.jsx
@@ -6,19 +6,19 @@ import Grid from '@mui/material/Grid2';
 function AppGrid() {
   return (
     <Grid container spacing={2}>
-    <Grid size={8}>
-      <Button variant='contained' fullWidth>1</Button>
+        <Grid size={{ xs: 6, md: 8 }}>
+        <Button variant='contained' fullWidth>1</Button>
+        </Grid>
+        <Grid size={{ xs: 6, md: 4 }}>
+        <Button variant='contained' fullWidth>2</Button>
+        </Grid>
+        <Grid size={{ xs: 6, md: 4 }}>
+        <Button variant='contained' fullWidth>3</Button>
+        </Grid>
+        <Grid size={{ xs: 6, md: 8 }}>
+        <Button variant='contained' fullWidth>4</Button>
+        </Grid>
     </Grid>
-    <Grid size={4}>
-      <Button variant='contained' fullWidth>2</Button>
-    </Grid>
-    <Grid size={4}>
-      <Button variant='contained' fullWidth>3</Button>
-    </Grid>
-    <Grid size={8}>
-      <Button variant='contained' fullWidth>4</Button>
-    </Grid>
-  </Grid>
   );
 };
 
