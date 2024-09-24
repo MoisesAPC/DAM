43e8055a6bf1927d992b0e34729e1cb9abb3aaa3
moisesPC <moisesapc64@gmail.com>
2024-09-24 08:59:55 +0100

    ahora uso intellij

--- PGV/procesos_prueba/src/procesos_prueba/main.java
+++ PGV/procesos_prueba/src/procesos_prueba/main.java
@@ -5,28 +5,27 @@ import java.io.IOException;
 public class main {
 
 	public static void main(String[] args) {
-		/*
-		String comando = "notepad.exe";
-		
-		// Lanza otro proceso ("notepad.exe") que es hijo de este .java
+		// USEFUL STATEMENT FOR KNOWING DE WORKING DIRECTORY FOR THE JAVA EXECUTABLE
+		// FOR DEBUGGING RELATIVE PATHS WITHIN THE PROGRAM
+		//System.out.println("Current working directory: " + System.getProperty("user.dir"));
+
+		// Use a relative path directly (assuming the DAM folder is opened on Intellij)
+		String fichero_bat = "PGV\\procesos_prueba\\src\\procesos_prueba\\prueba.bat";
+
 		try {
-			ProcessBuilder pb = new ProcessBuilder(comando);
+			// Create ProcessBuilder with the command and relative path
+			ProcessBuilder pb = new ProcessBuilder("cmd", "/C", "start", fichero_bat);
+
+			// Set the working directory to the project root
+			pb.directory(new java.io.File("."));
+
+			// Start the process
 			Process proceso = pb.start();
+
+			System.out.println("Process started successfully");
 		}
 		catch (IOException e) {
 			e.printStackTrace();
 		}
-		*/
-		
-		String fichero_bat = "src\\procesos_prueba\\prueba.bat";
-		
-		try {
-			ProcessBuilder pb = new ProcessBuilder("cmd", "/C", "start " + fichero_bat);
-			Process proceso = pb.start();
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-		
 	}
 }
