1767115cd338c17c68cbffb65a1e1354915cb5a3
moisesPC <moisesapc64@gmail.com>
2024-09-24 11:49:55 +0100

    sdfg

--- DAD/ProyectosReact/mianimal/src/App.jsx
+++ DAD/ProyectosReact/mianimal/src/App.jsx
@@ -9,7 +9,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import { createTheme } from '@mui/material/styles';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/500.css';io
 import '@fontsource/roboto/700.css';
 
 function App() {
