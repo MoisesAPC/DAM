4e1fcc137741721da5b83ec061c477bf289e4d2a
MoisesAPC <moipc647@gmail.com>
2024-11-11 23:46:08 +0000

    ddfg

--- PGV/UT2_ActividadEvaluacion/src/ut2_actividadEvaluacion_package/Main.java
+++ PGV/UT2_ActividadEvaluacion/src/ut2_actividadEvaluacion_package/Main.java
@@ -1,6 +1,7 @@
 package ut2_actividadEvaluacion_package;
 
 import java.util.Random;
+import java.util.concurrent.Semaphore;
 
 public class Main {
     private static final String[] LINEAS_AEREAS = {
@@ -17,17 +18,24 @@ public class Main {
     };
 
     public static void main(String[] args) {
-        TorreDeControl torreDeControl = new TorreDeControl();
         Random random = new Random();
+        // Dado que el único recurso compartido es la pista de despegue, ponemos un 1
+        Semaphore semaforo = new Semaphore(1);
 
-        while (true) {
+        // Creamos 100 vuelos de ejemplo. Cada uno se creará cada 500 milisegundos
+        for (int i = 0; i < 100; i++) {
             int aerolineaAleatoria = random.nextInt(LINEAS_AEREAS.length);
             String lineaAerea = LINEAS_AEREAS[aerolineaAleatoria];
-            Vuelo vuelo = new Vuelo(torreDeControl, lineaAerea);
-            vuelo.start();
+
+            Vuelo vuelo = new Vuelo(lineaAerea, semaforo);
+
+            // Ejecutamos todos los hilos de manera concurrente creando una nueva clase Thread y llamando a su
+            // método start()
+            Thread hilo = new Thread(vuelo);
+            hilo.start();
 
             try {
-                Thread.sleep(random.nextInt(3000) + 1000);
+                Thread.sleep(500);
             }
             catch (InterruptedException e) {
                 e.printStackTrace();
--- PGV/UT2_ActividadEvaluacion/src/ut2_actividadEvaluacion_package/Vuelo.java
+++ PGV/UT2_ActividadEvaluacion/src/ut2_actividadEvaluacion_package/Vuelo.java
@@ -1,38 +1,89 @@
 package ut2_actividadEvaluacion_package;
 
-class Vuelo extends Thread {
-    public TorreDeControl getTorreDeControl() {
-        return torreDeControl;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
+class Vuelo implements Runnable {
+    private final String aerolinea;
+    private final Semaphore semaforo;
+    // Recurso compartido por todos los hilos (de ahí que sea "static")
+    public static ArrayList<String> colaAerolineas = new ArrayList<>();
+
+    public void anadirVueloACola(String aerolinea) {
+        colaAerolineas.add(aerolinea);
     }
 
-    public void setTorreDeControl(TorreDeControl torreDeControl) {
-        this.torreDeControl = torreDeControl;
+    public void quitarVueloDeAerolinea(String aerolinea) {
+        colaAerolineas.remove(aerolinea);
     }
 
-    public String getNombre() {
-        return this.getName();
+    public void quitarPrimerVueloDeAerolinea() {
+        colaAerolineas.remove(0);
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public int obtenerPosicionEnColaDeVuelo(String aerolinea) {
+        return colaAerolineas.indexOf(aerolinea);
     }
 
-    private TorreDeControl torreDeControl;
-    private String nombre;
+    public Boolean colaLlena() {
+        return colaAerolineas.size() >= 50;
+    }
 
-    public Vuelo(TorreDeControl torreDeControl, String nombre) {
-        this.torreDeControl = torreDeControl;
-        this.nombre = nombre;
-        this.setName(nombre);
+    public int obtenerNumVuelosDeMismaAerolineaEsperando() {
+        int numVuelos = 0;
+
+        for (String vuelo : colaAerolineas) {
+            if (vuelo.equals(aerolinea)) {
+                numVuelos++;
+            }
+        }
+
+        return numVuelos;
+    }
+
+    Vuelo(String nombre, Semaphore semaforo) {
+        this.aerolinea = nombre;
+        this.semaforo = semaforo;
+    }
+
+    public void despegar() {
+        System.out.println(aerolinea + " ya despegó");
     }
 
     @Override
     public void run() {
         try {
-            torreDeControl.solicitarDespegue(getNombre());
+            Random random = new Random();
+
+            System.out.println(aerolinea + " pide despegar");
+            Thread.sleep(random.nextInt(5000) + 1000);
+
+            // Si la cola de aviones está llena, esperamos, sin acceder a la cola
+            while (colaLlena()) {
+                System.out.println("La cola ya tiene 50 plazas ocupadas. Esperaremos hasta que se desocupe la pista");
+                Thread.sleep(random.nextInt(3000) + 1000);
+            }
+
+            // Si hay más de 10 vuelos de la misma aerolínea, esperamos, sin acceder a la cola
+            while (obtenerNumVuelosDeMismaAerolineaEsperando() >= 10) {
+                System.out.println("Ya hay 10 vuelos de esta misma aerolínea esparando. Esperaremos hasta que los que queden puedan despegar");
+                Thread.sleep(random.nextInt(3000) + 1000);
+            }
+
+            anadirVueloACola(this.aerolinea);
+            System.out.println("El vuelo " + aerolinea + " se encuentra en pista en la posición " + obtenerPosicionEnColaDeVuelo(aerolinea));
+
+            // Zona crítica (acceso a recurso compartido): El avión ocupa la pista y despega.
+            // Al despegar, el siguiente vuelo pasa a ocupar la pista
+            semaforo.acquire();
+            Thread.sleep(random.nextInt(5000) + 1000);
+            despegar();
+            semaforo.release();
+            quitarPrimerVueloDeAerolinea();
         }
         catch (InterruptedException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 }
