cd4c5946030b50dacee3d9c3f5306bb1690af0d2
MoisesAPC <moipc647@gmail.com>
2024-12-18 07:55:19 +0000

    sdggfds

---
+++ AED/UT5_Actividad1/src/ut5_actividad1/EjemploDb4o.java
@@ -0,0 +1,32 @@
+package ut5_actividad1;
+
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+
+public class EjemploDb4o {
+	public static void main(String[] args) {
+
+		// Creamos registros
+		Personas p1 = new Personas(11, "42222222", "Ana", "Ronda");
+		Personas p2 = new Personas(12, "43333333", "Jose", "Arucas");
+		Personas p3 = new Personas(13, "45555555", "Luis", "Arucas");
+		Personas p4 = new Personas(14, "48888888", "Jorge", "Arucas");
+		Personas p5 = new Personas(15, "47777777", "Luisa", "Telde");
+		Personas p6 = new Personas(16, "49999999", "Pepa", "Telde");
+
+		// Abre la BD personas, si no existe, la crea
+		ObjectContainer db = Db4oEmbedded.openFile(
+				Db4oEmbedded.newConfiguration(), "bdpersonas.yap");
+
+		db.store(p1);
+		db.store(p2);
+		db.store(p3);
+		db.store(p4);
+		db.store(p5);
+		db.store(p6);
+		
+		//Se cierra la bd
+		db.close();
+
+	}
+}
---
+++ AED/UT5_Actividad1_Eclipse/src/ut5_actividad1_eclipse/ManipulaDb4o.java
@@ -0,0 +1,55 @@
+package ut5_actividad1_eclipse;
+
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+
+public class ManipulaDb4o {
+
+	public static void main(String[] args) {
+			// Abre la BD departamentos, si no existe, la crea
+			ObjectContainer db = Db4oEmbedded.openFile(
+					Db4oEmbedded.newConfiguration(), "bdpersonas.yap");
+			
+			//ObjectSet<Personas> p=db.queryByExample(new Personas(0,null,null,null));
+			ObjectSet<Personas> p=db.queryByExample(new Personas());
+			System.out.println("n� de personas: "+ p.size());
+			
+			while (p.hasNext()) {
+				Personas per=p.next();
+				System.out.println("Nombre : "+per.getNombre()+" "+"Direcci�n :" +per.getDomicilio());
+			}
+			
+			//Consulta de las personas que viven en Telde
+			p=db.queryByExample(new Personas(0,null,null,"Telde"));
+			System.out.println("n� de personas que viven en Telde: "+ p.size());
+			
+			while (p.hasNext()) {
+				Personas per=p.next();
+				System.out.println("Nombre : "+per.getNombre()+" "+"Direcci�n :" +per.getDomicilio());
+			}
+			
+			//Se modifica el domicilio de Luisa a Teror
+			p=db.queryByExample(new Personas(0,null,"Luisa",null));
+			if (p.size()==0) System.out.println("Luisa no existe");
+			else{
+				Personas per=p.next();
+				per.setDomicilio("Teror");	//se modifica
+				db.store(per);		//se vuelve a guardar
+			}
+			
+			
+			//Para eliminar al nif 43333333
+			p=db.queryByExample(new Personas(0,"43333333",null,null));
+			if (p.size()==0) System.out.println("Ese registro no exite");
+			else{
+				Personas per=p.next();
+				db.delete(per);		//se borra
+			}
+			
+			
+			
+			db.close();
+	}
+
+}
