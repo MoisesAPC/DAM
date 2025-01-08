cece988278526a7e61928cb6cca1e9ffd6ab96a2
MoisesAPC <moipc647@gmail.com>
2025-01-08 11:19:40 +0000

    sdfgsdgf

---
+++ AED/UT4_Actividad1/src/ut2_act_evaluacion/Main.java
@@ -0,0 +1,7 @@
+package ut2_act_evaluacion;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- AED/untitled/src/ut2_act_evaluacion/Main.java
+++
@@ -1,7 +0,0 @@
-package ut2_act_evaluacion;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
