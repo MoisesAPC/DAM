1b3b790556007d5475a8bb4b4cfd25928b6d9cdc
MoisesAPC <moipc647@gmail.com>
2025-01-09 09:54:39 +0000

    sdfgsdfg

--- DAD/ProyectosReact/testalumni/src/tests/TextBox2.test.jsx
+++ DAD/ProyectosReact/testalumni/src/tests/TextBox2.test.jsx
@@ -1,17 +1,17 @@
 import { render, screen } from "@testing-library/react";
-import TextBox from "../components/TextBox";
+import TextBox2 from "../components/TextBox2";
 import userEvent from "@testing-library/user-event";
 
-describe('TextBox componente', () => {
+describe('TextBox2 componente', () => {
 
     let box;
     let button;
     beforeEach(() => {
-        render(<TextBox />); //aquí generamos el virtual DOM
+        render(<TextBox2 />); //aquí generamos el virtual DOM
         //el name de Box lo definimos en su atributo title (fijarse en el código)
         box = screen.getByRole('caja', { name: 'es una caja' });
         //el name del botón es el propio título que le ponemos al botón
-        button = screen.getByRole('button', { name: 'Pulsa para modificar el color' });
+        button = screen.getByRole('button', { name: 'Pulsa para modificar el color del texto' });
     });
 
     it('la caja de texto se encuentra en el documento', () => {
@@ -20,19 +20,19 @@ describe('TextBox componente', () => {
     });
 
     it('el botón se encuentra en el documento', () => {
-        //comprobamos si la <Box> está en nuestro documento
+        //comprobamos si el botón está en nuestro documento
         expect(button).toBeInTheDocument();
     });
     
-    it('la caja con el texto tiene un color inicial', () => {
+    it('la caja con el texto tiene un color de texto inicial', () => {
   
-        //comprobamos si el Box tiene un color de fondo indigo
+        //comprobamos si el Box tiene un color de texto rosa
         expect(box).toHaveStyle({
-            backgroundColor: 'rgb(255,255,0)'
+            color: 'rgb(255,192,203)'
         });
     });
     
-    it('comprobar si al pulsar el botón cambia el botón de fondo', async () => {
+    it('comprobar si al pulsar el botón cambia el color del texto', async () => {
      
         //usamos el userEvent. Lo primero que debemos hacer es inicializarlo:
         const user = userEvent.setup()
@@ -41,7 +41,7 @@ describe('TextBox componente', () => {
         //Ahora comprobamos que al pulsar en el botón se cambia
         //el color de fondo de la caja (Box) a tomato.
         expect(box).toHaveStyle({
-            backgroundColor: 'rgb(255,0,0)'
+            color: 'rgb(0,0,0)'
         });
     });
 });
